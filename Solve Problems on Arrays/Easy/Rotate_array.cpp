e43ea7f0c3ced6302c6cc8eebfda48c7bcb7a2b2
Nish613 <nishtha2711@gmail.com>
2026-05-29 22:10:00 +0530

    Implement array rotation in Rotate_array.cpp

---
+++ Solve Problems on Arrays/Easy/Rotate_array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        k=k%n;
+        vector<int> temp = nums;
+        for(int i=0;i<k;i++)
+        {
+            temp[i]=nums[n-k+i];
+        }
+        for(int i=k;i<n;i++)
+        {
+            temp[i] = nums[i-k];
+        }
+        nums=temp;
+    }
+};

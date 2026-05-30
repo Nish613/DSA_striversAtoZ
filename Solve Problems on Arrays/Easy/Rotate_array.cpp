04fc2e08443a355513760014c86538d005f5c401
Nish613 <nishtha2711@gmail.com>
2026-05-30 22:27:54 +0530

    Add optimal recursive array rotation method
    Implemented an optimal approach for rotating an array using recursion.

--- Solve Problems on Arrays/Easy/Rotate_array.cpp
+++ Solve Problems on Arrays/Easy/Rotate_array.cpp
@@ -15,3 +15,30 @@ public:
         nums=temp;
     }
 };
+
+
+//Optimal approach
+class Solution {
+public:
+
+    void rec(vector<int>& nums, int l,int h,int k)
+    {
+        while(l<h)
+        {
+            int temp=nums[l];
+            nums[l]=nums[h-1];
+            nums[h-1]=temp;
+            l++;
+            h--;
+        }
+    }
+    
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        k=k%n;
+        rec(nums,0,n,k);
+        rec(nums,0,k,k);
+        rec(nums,k,n,k);
+        
+    }
+};

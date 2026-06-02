ccca40488c9d9857d88bbb3b6e2dc256d2e7c71a
Nish613 <nishtha2711@gmail.com>
2026-06-02 21:44:53 +0530

    Implement optimal union of two arrays

--- Solve Problems on Arrays/Easy/Union_array.cpp
+++ Solve Problems on Arrays/Easy/Union_array.cpp
@@ -35,3 +35,50 @@ public:
         return temp;
     }
 };
+
+
+//Optimal approach
+class Solution {
+public:
+    vector<int> unionArray(vector<int>& nums1, vector<int>& nums2) {
+        int i=0,j=0;
+        int n1=nums1.size();
+        int n2=nums2.size();
+        vector<int> temp;
+        while(i<n1&&j<n2)
+        {
+            if(nums1[i]<nums2[j])
+            {
+                if(temp.empty()||temp.back()!=nums1[i])
+                temp.push_back(nums1[i]);
+                i++;
+            }
+            else if(nums1[i]>nums2[j])
+            {
+                if(temp.empty()||temp.back()!=nums2[j])
+                temp.push_back(nums2[j]);
+                j++;
+            }
+            else if(nums1[i]==nums2[j])
+            {
+                if(temp.empty()||temp.back()!=nums1[i])
+                temp.push_back(nums1[i]);
+                i++;
+                j++;
+            }
+        }
+        while(i<n1)
+        {
+            if(temp.empty()||temp.back()!=nums1[i])
+            temp.push_back(nums1[i]);
+            i++;
+        }
+        while(j<n2)
+        {
+            if(temp.empty()||temp.back()!=nums2[j])
+            temp.push_back(nums2[j]);
+            j++;
+        }
+        return temp;
+    }
+};
